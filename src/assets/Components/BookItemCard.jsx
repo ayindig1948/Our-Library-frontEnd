b91d1c8b5bfde7b5ea16aebc316351fee7bac00b
avrum indig <ayindig1948@gmail>
2026-06-21 09:44:28 -0400

    .

--- src/assets/Components/BookItemCard.jsx
+++ src/assets/Components/BookItemCard.jsx
@@ -9,7 +9,7 @@ const BookItemCard = ({book,Fulfill}) => {
             </p>
             <p className="text-gray-700 dark:text-gray-300">{book.userId}</p>
             
-            <button className="mt-2 w-fit rounded-lg bg-purple-600 px-4 py-2 text-sm font-medium text-white transition hover:bg-purple-700" onClick={() => Fulfill(book)}>
+            <button className="cursor-pointer  mt-2 w-fit rounded-lg bg-purple-600 px-4 py-2 text-sm font-medium text-white transition hover:bg-purple-700" onClick={() => Fulfill(book)}>
                Fulfill book
             </button>
         </div>
